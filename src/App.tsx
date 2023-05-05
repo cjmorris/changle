62f5aacea100bfbcc7d4fafe280318253aa9dbfb
Cameron Morris <cameronjmorris87@gmail.com>
2023-05-05 13:06:47 +1200

    added randomized answer list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header"
 import ChangleGame from "./components/ChangleGame"
+import ANSWER_LIST from "./word_lists/answer_list"
 
 export default function App() {
+  const answerIndex: number = Math.floor(Math.random()*ANSWER_LIST.length)
+  const answerWord = ANSWER_LIST[answerIndex]
+
   return (
     <>
       <Header/>
-      <ChangleGame answer="DOORS"/>
+      <ChangleGame answer={answerWord.toUpperCase()}/>
     </>
   )
 }
